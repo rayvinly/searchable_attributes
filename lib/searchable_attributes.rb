3987d1e183773ab78a2bc539e60bb1c89ce3771f
Raymond Law <rayvinly@gmail.com>
2009-02-27 13:19:32 -0500

    Added several improvements
    - Automatically create named_scopes for each column name in the model, so no more configuration
    - Remove the _in named_scope (it was wrong anyway) since it is covered with the _equal named_scope by passing an array
    - Fixed all specs

--- lib/searchable_attributes.rb
+++ lib/searchable_attributes.rb
@@ -3,9 +3,8 @@ module SearchableAttributes
     self.columns.each do |column|
       case column.type
       when :integer, :float, :decimal, :datetime, :date, :time
-        self.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => [ "#{column.name} = ?", value ] } }
+        self.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => { column.name => value } } }
         self.named_scope "with_#{column.name}_equal_if_not_null".to_sym, lambda { |value| { :conditions => ([ "#{column.name} = ?", value ] unless value.blank?) } }
-        self.named_scope "with_#{column.name}_in".to_sym, lambda { |values| { :conditions => ([ "#{column.name} in (?)", values.join(',') ] unless values.blank?) } }
         self.named_scope "with_#{column.name}_within_inclusive".to_sym, lambda { |range| { :conditions => ([ "#{column.name} >= ? AND #{column.name} <= ?", range.begin, range.end ] unless range.blank?) } }
         self.named_scope "with_#{column.name}_within_exclusive".to_sym, lambda { |range| { :conditions => ([ "#{column.name} > ? AND #{column.name} < ?", range.begin, range.end ] unless range.blank?) } }
         self.named_scope "with_#{column.name}_greater_than".to_sym, lambda { |value| { :conditions => ([ "#{column.name} > ?", value ] unless value.blank?) } }
@@ -13,9 +12,8 @@ module SearchableAttributes
         self.named_scope "with_#{column.name}_greater_than_or_equal_to".to_sym, lambda { |value| { :conditions => ([ "#{column.name} >= ?", value ] unless value.blank?) } }
         self.named_scope "with_#{column.name}_smaller_than_or_equal_to".to_sym, lambda { |value| { :conditions => ([ "#{column.name} <= ?", value ] unless value.blank?) } }
       when :string, :text
-        self.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => [ "#{column.name} = ?", value ] } }
+        self.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => { column.name => value } } }
         self.named_scope "with_#{column.name}_equal_if_not_null".to_sym, lambda { |value| { :conditions => ([ "#{column.name} = ?", value ] unless value.blank?) } }
-        self.named_scope "with_#{column.name}_in".to_sym, lambda { |values| { :conditions => ([ "#{column.name} in (?)", values.join(',') ] unless values.blank?) } }
         self.named_scope "with_#{column.name}_like".to_sym, lambda { |value| { :conditions => ([ "#{column.name} like ?", "%#{value}%" ] unless value.blank?) } }
         self.named_scope "with_#{column.name}_start_with".to_sym, lambda { |value| { :conditions => ([ "#{column.name} like ?", "#{value}%" ] unless value.blank?) } }
         self.named_scope "with_#{column.name}_end_with".to_sym, lambda { |value| { :conditions => ([ "#{column.name} like ?", "%#{value}" ] unless value.blank?) } }
--- spec/searchable_attributes_spec.rb
+++ spec/searchable_attributes_spec.rb
@@ -9,34 +9,164 @@ describe SearchableAttributes do
     SearchableUser.create(fixture[1].symbolize_keys)
   end
 
-  it "should define named_scope for a single exact attribute" do
-    SearchableUser.methods.select { |method| method == 'with_middle_name' }.size.should == 1
-    SearchableUser.methods.include?('with_middle_name').should be_true
+  it "should define named_scope for string attributes" do
+    SearchableUser.methods.include?('with_first_name_equal').should be_true
+    SearchableUser.methods.include?('with_first_name_equal_if_not_null').should be_true
+    SearchableUser.methods.include?('with_first_name_like').should be_true
+    SearchableUser.methods.include?('with_first_name_start_with').should be_true
+    SearchableUser.methods.include?('with_first_name_end_with').should be_true
   end
 
-  it "should define named_scope for an array of exact attributes" do
-    SearchableUser.methods.include?('with_first_name').should be_true
-    SearchableUser.methods.include?('with_last_name').should be_true
+  it "should define named_scope for boolean attributes" do
+    SearchableUser.methods.include?('with_injured_equal').should be_true
+    SearchableUser.methods.include?('with_injured_equal_if_not_null').should be_true
   end
 
-  it "should define named_scope for a single like attribute" do
-    SearchableUser.methods.include?('with_state_like').should be_true
+  it "should define named_scope for integer attributes" do
+    SearchableUser.methods.include?('with_age_equal').should be_true
+    SearchableUser.methods.include?('with_age_equal_if_not_null').should be_true
+    SearchableUser.methods.include?('with_age_within_inclusive').should be_true
+    SearchableUser.methods.include?('with_age_within_exclusive').should be_true
+    SearchableUser.methods.include?('with_age_greater_than').should be_true
+    SearchableUser.methods.include?('with_age_smaller_than').should be_true
+    SearchableUser.methods.include?('with_age_greater_than_or_equal_to').should be_true
+    SearchableUser.methods.include?('with_age_smaller_than_or_equal_to').should be_true
   end
 
-  it "should define named_scope for an array of like attributes" do
-    SearchableUser.methods.include?('with_address_like').should be_true
-    SearchableUser.methods.include?('with_city_like').should be_true
+  it "should define named_scope for decimal attributes" do
+    SearchableUser.methods.include?('with_salary_equal').should be_true
+    SearchableUser.methods.include?('with_salary_equal_if_not_null').should be_true
+    SearchableUser.methods.include?('with_salary_within_inclusive').should be_true
+    SearchableUser.methods.include?('with_salary_within_exclusive').should be_true
+    SearchableUser.methods.include?('with_salary_greater_than').should be_true
+    SearchableUser.methods.include?('with_salary_smaller_than').should be_true
+    SearchableUser.methods.include?('with_salary_greater_than_or_equal_to').should be_true
+    SearchableUser.methods.include?('with_salary_smaller_than_or_equal_to').should be_true
   end
 
-  it "should find records with exact searchable attributes" do
-    SearchableUser.with_first_name('Gilbert').each { |searchable_user| searchable_user.first_name.should == 'Gilbert' }
-    SearchableUser.with_middle_name('James').each { |searchable_user| searchable_user.middle_name.should == 'James' }
-    SearchableUser.with_last_name('Butler').each { |searchable_user| searchable_user.last_name.should == 'Butler' }
+  it "should define named_scope for datetime attributes" do
+    SearchableUser.methods.include?('with_join_on_equal').should be_true
+    SearchableUser.methods.include?('with_join_on_equal_if_not_null').should be_true
+    SearchableUser.methods.include?('with_join_on_within_inclusive').should be_true
+    SearchableUser.methods.include?('with_join_on_within_exclusive').should be_true
+    SearchableUser.methods.include?('with_join_on_greater_than').should be_true
+    SearchableUser.methods.include?('with_join_on_smaller_than').should be_true
+    SearchableUser.methods.include?('with_join_on_greater_than_or_equal_to').should be_true
+    SearchableUser.methods.include?('with_join_on_smaller_than_or_equal_to').should be_true
   end
 
-  it "should find records with like searchable attributes" do
-    SearchableUser.with_address_like('Main').each { |searchable_user| searchable_user.address.include?('Main') }
-    SearchableUser.with_city_like('Great').each { |searchable_user| searchable_user.address.include?('Great') }
-    SearchableUser.with_state_like('Columbia').each { |searchable_user| searchable_user.address.include?('Columbia') }
+  it "should find records for string attributes" do
+    SearchableUser.with_first_name_equal('Gilbert').count.should > 0
+    SearchableUser.with_first_name_equal('Gilbert').each { |searchable_user| searchable_user.first_name.should == 'Gilbert' }
+
+    SearchableUser.with_first_name_equal_if_not_null('Gilbert').count.should > 0
+    SearchableUser.with_first_name_equal_if_not_null('Gilbert').each { |searchable_user| searchable_user.first_name.should == 'Gilbert' }
+
+    SearchableUser.with_first_name_equal_if_not_null(nil).count.should > 0
+    SearchableUser.with_first_name_equal_if_not_null(nil).count.should == SearchableUser.count
+
+    SearchableUser.with_first_name_like('ilbe').count.should > 0
+    SearchableUser.with_first_name_like('ilbe').each { |searchable_user| searchable_user.first_name.include?('ilbe') }
+
+    SearchableUser.with_position_start_with('P').count.should > 0
+    SearchableUser.with_position_start_with('P').each { |searchable_user| searchable_user.position.should =~ /^P/ }
+
+    SearchableUser.with_position_end_with('Forward').count.should > 0
+    SearchableUser.with_position_end_with('Forward').each { |searchable_user| searchable_user.position.should =~ /Forward$/ }
+  end
+
+  it "should find records for boolean attributes" do
+    SearchableUser.with_injured_equal(true).count.should > 0
+    SearchableUser.with_injured_equal(true).each { |searchable_user| searchable_user.injured.should be_true }
+
+    SearchableUser.with_injured_equal(false).count.should > 0
+    SearchableUser.with_injured_equal(false).each { |searchable_user| searchable_user.injured.should be_false }
+  end
+
+  it "should find records for integer attributes" do
+    SearchableUser.with_age_equal(26).count.should > 0
+    SearchableUser.with_age_equal(26).each { |searchable_user| searchable_user.age.should == 26 }
+
+    SearchableUser.with_age_equal_if_not_null(26).count.should > 0
+    SearchableUser.with_age_equal_if_not_null(26).each { |searchable_user| searchable_user.age.should == 26 }
+
+    SearchableUser.with_age_equal_if_not_null(nil).count.should > 0
+    SearchableUser.with_age_equal_if_not_null(nil).count.should == SearchableUser.count
+
+    SearchableUser.with_age_within_inclusive((26..30)).count.should > 0
+    SearchableUser.with_age_within_inclusive((26..30)).each { |searchable_user| (26..30).include?(searchable_user.age).should be_true }
+
+    SearchableUser.with_age_within_exclusive((26..30)).count.should > 0
+    SearchableUser.with_age_within_exclusive((26..30)).each { |searchable_user| (26..30).include?(searchable_user.age).should be_true }
+
+    SearchableUser.with_age_greater_than(28).count.should > 0
+    SearchableUser.with_age_greater_than(28).each { |searchable_user| searchable_user.age.should > 28 }
+
+    SearchableUser.with_age_smaller_than(28).count.should > 0
+    SearchableUser.with_age_smaller_than(28).each { |searchable_user| searchable_user.age.should < 28 }
+
+    SearchableUser.with_age_greater_than_or_equal_to(28).count.should > 0
+    SearchableUser.with_age_greater_than_or_equal_to(28).each { |searchable_user| searchable_user.age.should >= 28 }
+
+    SearchableUser.with_age_smaller_than_or_equal_to(28).count.should > 0
+    SearchableUser.with_age_smaller_than_or_equal_to(28).each { |searchable_user| searchable_user.age.should <= 28 }
+  end
+
+  it "should find records for decimal attributes" do
+    SearchableUser.with_salary_equal(33333.33).count.should > 0
+    SearchableUser.with_salary_equal(33333.33).each { |searchable_user| searchable_user.salary.should == 33333.33 }
+
+    SearchableUser.with_salary_equal_if_not_null(33333.33).count.should > 0
+    SearchableUser.with_salary_equal_if_not_null(33333.33).each { |searchable_user| searchable_user.salary.should == 33333.33 }
+
+    SearchableUser.with_salary_equal_if_not_null(nil).count.should > 0
+    SearchableUser.with_salary_equal_if_not_null(nil).count.should == SearchableUser.count
+
+    SearchableUser.with_salary_within_inclusive((33333.33..99999.99)).count.should > 0
+    SearchableUser.with_salary_within_inclusive((33333.33..99999.99)).each { |searchable_user| (33333.33..99999.99).include?(searchable_user.salary).should be_true }
+
+    SearchableUser.with_salary_within_exclusive((33333.33..99999.99)).count.should > 0
+    SearchableUser.with_salary_within_exclusive((33333.33..99999.99)).each { |searchable_user| (33333.33..99999.99).include?(searchable_user.salary).should be_true }
+
+    SearchableUser.with_salary_greater_than(25000).count.should > 0
+    SearchableUser.with_salary_greater_than(25000).each { |searchable_user| searchable_user.salary.should > 25000 }
+
+    SearchableUser.with_salary_smaller_than(75000).count.should > 0
+    SearchableUser.with_salary_smaller_than(75000).each { |searchable_user| searchable_user.salary.should < 75000 }
+
+    SearchableUser.with_salary_greater_than_or_equal_to(66666.66).count.should > 0
+    SearchableUser.with_salary_greater_than_or_equal_to(66666.66).each { |searchable_user| searchable_user.salary.should >= 66666.66 }
+
+    SearchableUser.with_salary_smaller_than_or_equal_to(66666.66).count.should > 0
+    SearchableUser.with_salary_smaller_than_or_equal_to(66666.66).each { |searchable_user| searchable_user.salary.should <= 66666.66 }
+  end
+
+  it "should find records for date attributes" do
+    SearchableUser.with_join_on_equal('2006-02-27'.to_date).count.should > 0
+    SearchableUser.with_join_on_equal('2006-02-27'.to_date).each { |searchable_user| searchable_user.join_on.should == 3.years.ago.to_date }
+
+    SearchableUser.with_join_on_equal_if_not_null('2006-02-27'.to_date).count.should > 0
+    SearchableUser.with_join_on_equal_if_not_null('2006-02-27'.to_date).each { |searchable_user| searchable_user.join_on.should == 3.years.ago.to_date }
+
+    SearchableUser.with_join_on_equal_if_not_null(nil).count.should > 0
+    SearchableUser.with_join_on_equal_if_not_null(nil).count.should == SearchableUser.count
+
+    SearchableUser.with_join_on_within_inclusive(('2004-02-27'.to_date..'2006-02-27'.to_date)).count.should > 0
+    SearchableUser.with_join_on_within_inclusive(('2004-02-27'.to_date..'2006-02-27'.to_date)).each { |searchable_user| ('2004-02-27'.to_date..'2006-02-27'.to_date).include?(searchable_user.join_on).should be_true }
+
+    SearchableUser.with_join_on_within_exclusive(('2003-02-27'.to_date..'2006-02-27'.to_date)).count.should > 0
+    SearchableUser.with_join_on_within_exclusive(('2003-02-27'.to_date..'2006-02-27'.to_date)).each { |searchable_user| ('2003-02-27'.to_date..'2006-02-27'.to_date).include?(searchable_user.join_on).should be_true }
+
+    SearchableUser.with_join_on_greater_than('2004-02-27'.to_date).count.should > 0
+    SearchableUser.with_join_on_greater_than('2004-02-27'.to_date).each { |searchable_user| searchable_user.join_on.should > '2004-02-27'.to_date }
+
+    SearchableUser.with_join_on_smaller_than('2006-02-27'.to_date).count.should > 0
+    SearchableUser.with_join_on_smaller_than('2006-02-27'.to_date).each { |searchable_user| searchable_user.join_on.should < '2006-02-27'.to_date }
+
+    SearchableUser.with_join_on_greater_than_or_equal_to('2006-02-27'.to_date).count.should > 0
+    SearchableUser.with_join_on_greater_than_or_equal_to('2006-02-27'.to_date).each { |searchable_user| searchable_user.join_on.should >= '2006-02-27'.to_date }
+
+    SearchableUser.with_join_on_smaller_than_or_equal_to('2006-02-27'.to_date).count.should > 0
+    SearchableUser.with_join_on_smaller_than_or_equal_to('2006-02-27'.to_date).each { |searchable_user| searchable_user.join_on.should <= '2006-02-27'.to_date }
   end
 end
