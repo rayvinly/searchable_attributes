017bc1d14ef34ed280267e01e4ec08b28e05aefd
Raymond Law <rayvinly@gmail.com>
2009-07-01 16:17:48 -0400

    Added table name to generated SQL queries to avoid column conflicts when multiple tables have the same column name

--- lib/searchable_attributes.rb
+++ lib/searchable_attributes.rb
@@ -15,20 +15,20 @@ module SearchableAttributes
       when :integer, :float, :decimal, :datetime, :date, :time
         model.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => { column.name => value } } }
         model.named_scope "with_#{column.name}_equal_if_not_blank".to_sym, lambda { |value| { :conditions => ({ column.name => value } unless value.blank?) } }
-        model.named_scope "with_#{column.name}_within_inclusive".to_sym, lambda { |range| { :conditions => ([ "#{column.name} >= ? AND #{column.name} <= ?", range.begin, range.end ] unless range.blank?) } }
-        model.named_scope "with_#{column.name}_within_exclusive".to_sym, lambda { |range| { :conditions => ([ "#{column.name} > ? AND #{column.name} < ?", range.begin, range.end ] unless range.blank?) } }
-        model.named_scope "with_#{column.name}_greater_than".to_sym, lambda { |value| { :conditions => ([ "#{column.name} > ?", value ] unless value.blank?) } }
-        model.named_scope "with_#{column.name}_smaller_than".to_sym, lambda { |value| { :conditions => ([ "#{column.name} < ?", value ] unless value.blank?) } }
-        model.named_scope "with_#{column.name}_greater_than_or_equal_to".to_sym, lambda { |value| { :conditions => ([ "#{column.name} >= ?", value ] unless value.blank?) } }
-        model.named_scope "with_#{column.name}_smaller_than_or_equal_to".to_sym, lambda { |value| { :conditions => ([ "#{column.name} <= ?", value ] unless value.blank?) } }
+        model.named_scope "with_#{column.name}_within_inclusive".to_sym, lambda { |range| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} >= ? AND `#{model.to_s.tableize}`.#{column.name} <= ?", range.begin, range.end ] unless range.blank?) } }
+        model.named_scope "with_#{column.name}_within_exclusive".to_sym, lambda { |range| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} > ? AND `#{model.to_s.tableize}`.#{column.name} < ?", range.begin, range.end ] unless range.blank?) } }
+        model.named_scope "with_#{column.name}_greater_than".to_sym, lambda { |value| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} > ?", value ] unless value.blank?) } }
+        model.named_scope "with_#{column.name}_smaller_than".to_sym, lambda { |value| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} < ?", value ] unless value.blank?) } }
+        model.named_scope "with_#{column.name}_greater_than_or_equal_to".to_sym, lambda { |value| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} >= ?", value ] unless value.blank?) } }
+        model.named_scope "with_#{column.name}_smaller_than_or_equal_to".to_sym, lambda { |value| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} <= ?", value ] unless value.blank?) } }
       when :string, :text
         model.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => { column.name => value } } }
         model.named_scope "with_#{column.name}_equal_if_not_blank".to_sym, lambda { |value| { :conditions => ({ column.name => value } unless value.blank?) } }
-        model.named_scope "with_#{column.name}_like".to_sym, lambda { |value| { :conditions => ([ "#{column.name} like ?", "%#{value}%" ] unless value.blank?) } }
-        model.named_scope "with_#{column.name}_start_with".to_sym, lambda { |value| { :conditions => ([ "#{column.name} like ?", "#{value}%" ] unless value.blank?) } }
-        model.named_scope "with_#{column.name}_end_with".to_sym, lambda { |value| { :conditions => ([ "#{column.name} like ?", "%#{value}" ] unless value.blank?) } }
+        model.named_scope "with_#{column.name}_like".to_sym, lambda { |value| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} like ?", "%#{value}%" ] unless value.blank?) } }
+        model.named_scope "with_#{column.name}_start_with".to_sym, lambda { |value| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} like ?", "#{value}%" ] unless value.blank?) } }
+        model.named_scope "with_#{column.name}_end_with".to_sym, lambda { |value| { :conditions => ([ "`#{model.to_s.tableize}`.#{column.name} like ?", "%#{value}" ] unless value.blank?) } }
       when :boolean
-        model.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => [ "#{column.name} = ?", value ] } }
+        model.named_scope "with_#{column.name}_equal".to_sym, lambda { |value| { :conditions => [ "`#{model.to_s.tableize}`.#{column.name} = ?", value ] } }
         model.named_scope "with_#{column.name}_equal_if_not_blank".to_sym, lambda { |value| { :conditions => ({ column.name => value } unless value.blank?) } }
       end
     end
